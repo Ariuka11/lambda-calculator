86d44e6f3a7777d3b54db74e43ad6c7aa8e3d1cd
Ariunbold <ariun1118@gmail.com>
2019-08-25 17:39:39 -0400

    MVP AND couple Stretch goal finished

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,35 @@
-import React from "react";
-
+import React, {useState}  from "react";
+import SpecialButton from './SpecialButton'
 //import any components needed
-
+import {specials} from '../../../data';
 //Import your array data to from the provided data file
 
-const Specials = () => {
+const Specials = ({ updateValue, resetValue, backSpace }) => {
   // STEP 2 - add the imported data to state
+ const [specialsState] = useState(specials)
 
+ const clickSpecialButton = button => {
+   button === 'C' ? resetValue() : updateValue (button)
+ }
+ const clickBackSpace = button => {
+   if(button === '<=') {
+    return backSpace()
+   }
+ }
+ 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {specialsState.map( special => {
+        return <SpecialButton 
+          special = {special} 
+          updateValue= {() => clickSpecialButton(special)}
+          backSpace = {() => clickBackSpace(special)} />
+       })}
     </div>
   );
 };
+
+export default Specials;
